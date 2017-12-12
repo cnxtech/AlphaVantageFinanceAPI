faffcff5a274b8b261e4de30ee9c250679412d2e
Shayan Ushani <shayanushani@gmail.com>
2017-12-12 18:33:53 -0500

    Update AlphaVantage Technical Indicators.py

--- AlphaVantage Technical Indicators.py
+++ AlphaVantage Technical Indicators.py
@@ -11,42 +11,42 @@ import matplotlib.pyplot as plt
 matplotlib.rcParams['figure.figsize'] = (15, 10)
 
 #Plotting Bollinger Bands MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_bbands(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Bollinger Bands for  $MSFT: 1 min)')
 plt.show()
 
 #Plotting RSI MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_rsi(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Relative Strength Index (RSI) for  $MSFT: 1 min)')
 plt.show()
 
 #Plotting OBV MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_obv(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Relative Strength Index (OBV) for  $MSFT: 1 min)')
 plt.show()
 
 #Plotting Average Directional Movement Index MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_adx(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Average Directional Movement Index (ADX) for  $MSFT: 1 min)')
 plt.show()
 
 #Plotting Stochoastic Oscillator MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_stoch(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Stochoastic Oscillator (STOCH) for  $MSFT: 1 min)')
 plt.show()
 
 #Plotting Simple Moving Average MSFT
-ti = TechIndicators(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+ti = TechIndicators(key='YOUR_KEY', output_format='pandas')
 data, meta_data = ti.get_sma(symbol='MSFT', interval='1min',)
 data.plot()
 plt.title('Simple Moving Average (SMA) for  $MSFT: 1 min)')
