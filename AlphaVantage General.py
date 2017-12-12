6019f84a888fd1e832ef3ccd95877d48e0c214ae
Shayan Ushani <shayanushani@gmail.com>
2017-12-12 18:30:59 -0500

    Add files via upload

---
+++ AlphaVantage General.py
@@ -0,0 +1,48 @@
+from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+from alpha_vantage.sectorperformance import SectorPerformances
+from alpha_vantage.cryptocurrencies import CryptoCurrencies
+import matplotlib
+import matplotlib.pyplot as plt
+
+#Plotting & General Exploring into Alpha Vantage's API
+
+#Size of charts
+matplotlib.rcParams['figure.figsize'] = (15, 10)
+
+#Plotting MSFT's returns 60min interval
+ts = TimeSeries(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='60min', outputsize='full')
+data['close'].plot()
+plt.title('Intraday Times Series for $MSFT: Hourly')
+plt.show()
+
+#Plotting SectorPerformances
+sp = SectorPerformances(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+data, meta_data = sp.get_sector()
+data['Rank A: Real-Time Performance'].plot(kind='bar')
+plt.title('Real Time Performance (%) per Sector')
+plt.tight_layout()
+plt.grid()
+plt.show()
+
+#Forex
+cc = ForeignExchange(key=os.environ['W9XB0WD5FGMNUK5H'])
+data, _ = cc.get_currency_exchange_rate(from_currency='BTC',to_currency='USD')
+print(data)
+
+#Plotting CryptoCurrency: BitCoin
+cc = CryptoCurrencies(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+data, meta_data = cc.get_digital_currency_intraday(symbol='BTC', market='CNY')
+data['. price (USD)'].plot()
+plt.tight_layout()
+plt.title('Intraday value for bitcoin (BTC)')
+plt.grid()
+plt.show()
+
+#Error with AlphaVantage API not adjusting for splits with $APPL
+ts = TimeSeries(key='W9XB0WD5FGMNUK5H', output_format='pandas')
+data, meta_data = ts.get_daily_adjusted(symbol='AAPL', outputsize='full')
+data['close'].plot()
+plt.title('Daily Adjusted for $AAPL')
+plt.show()
